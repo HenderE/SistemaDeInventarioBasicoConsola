8389a23c9ee2195ccb45dcbdd2bce0906c51dc07
Hender Escorcia <hender_escorcia@hotmail.com>
2026-03-20 14:29:11 -0500

    Documentamos codigo y correjimos

--- src/gestion.py
+++ src/gestion.py
@@ -1,3 +1,42 @@
+#creo funcion para agregar producto y luego llamar la funcion en su respectiva opcion  
+def agregar_producto():
+            #pido los datos del producto mediante una funcion
+            nombre = input("\nIngrese nombre del producto: ")
+            precio = float(input("\nIngrese el precio del producto: "))
+            cantidad = int(input("\nIngrese la cantidad del producto: "))
+            producto = {
+                "Nombre": nombre,
+                "Precio": precio,
+                "Cantidad": cantidad
+            }
+            #agrego el producto pedido
+            inventario.append(producto)
+            print("\nProducto agregado correctamente.")
+#creo funcion para mostrar producto y luego llamar la funcion en su respectiva opcion para mostrar al usuario
+def mostrar_producto():
+            #verifico si el inventario esta vacio
+            if len(inventario) == 0:
+                print("")
+                print("\nInventario Vacio.")
+            else:
+                print("")
+                print("------------------ Inventario ------------------")
+                #recorro la lista y muestro cada producto
+                for producto in inventario:
+                    print(f"\nProducto: {producto['Nombre']} | Precio: {producto['Precio']} | Cantidad: {producto['Cantidad']}")
+#creo funcion para calcular el producto y luego llamar la funcion en su respectiva opcion
+def calcular_producto():
+            #hacemos el calculo de las estadisticas del inventario
+            total = 0
+            cantidad_total = 0
+            #recorro el inventario para calcular el valor total y cantidad total
+            for producto in inventario:
+                total += producto["Precio"] * producto["Cantidad"]
+                cantidad_total += producto["Cantidad"]
+            #muestro los resultados
+            print(f"\nValor total del inventario: {total}")
+            print(f"\nCantidad total de productos: {cantidad_total}")
+
 #creo una variale llamada inventario donde guardo varios productos
 #cada producto es un diccionario con su nombre, precio y cantidad
 inventario = [
@@ -22,42 +61,18 @@ while True:
     try:
         #le pido al usuario que ingrese una opción
         opcion = int(input("\nIngrese una opción: "))
-        #opcion1: pido los datos del producto
+        
+        #opciones disponibles
         if opcion == 1:
-            nombre = input("\nIngrese nombre del producto: ")
-            precio = float(input("\nIngrese el precio del producto: "))
-            cantidad = int(input("\nIngrese la cantidad del producto: "))
-            producto = {
-                "Nombre": nombre,
-                "Precio": precio,
-                "Cantidad": cantidad
-            }
-            #agrego el producto pedido
-            inventario.append(producto)
-            print("\nProducto agregado correctamente.")
-        #opcion2: muestro los productos del inventario 
+            agregar_producto()
+        
         elif opcion == 2:
-            #verifico si el inventario esta vacio
-            if len(inventario) == 0:
-                print("")
-                print("\nInventario Vacio.")
-            else:
-                print("")
-                print("------------------ Inventario ------------------")
-                #recorro la lista y muestro cada producto
-                for producto in inventario:
-                    print(f"\nProducto: {producto['Nombre']} | Precio: {producto['Precio']} | Cantidad: {producto['Cantidad']}")
-        #opcion3: hacemos el calculo de las estadisticas del inventario
+            
+            mostrar_producto()
+        
         elif opcion == 3:
-            total = 0
-            cantidad_total = 0
-            #recorro el inventario para calcular el valor total y cantidad total
-            for producto in inventario:
-                total += producto["Precio"] * producto["Cantidad"]
-                cantidad_total += producto["Cantidad"]
-            #muestro los resultados
-            print(f"\nValor total del inventario: {total}")
-            print(f"\nCantidad total de productos: {cantidad_total}")
+            calcular_producto()
+
         #opcion4: salir del programa
         elif opcion == 4:
             print("\nSaliendo del sistema...")
@@ -68,3 +83,11 @@ while True:
     except ValueError:
         #manejanmos el error si el usuario no ingresa un numero valido
         print("\nIngresa un dato valido.")
+
+
+    #----------------------------------------Comentario Final----------------------------------------
+    """El objetivo de esta semana era realizar el feature 2 de la epic en la cual estamos trabajando.
+    registramos valores como nombre, precio y cantidad, trabajamos con listas, diccionarios dentro de
+    una lista y tambien trabajamos dentro de un ciclo infinito con opcion para salir del programa.
+    creamos un inventario el cual agrega producto, visualiza el producto y calcula las estadisticas del
+    inventario mostrandoselas al cliente."""
